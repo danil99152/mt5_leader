8c69e8b936d87521522c3e09f8b2eb5c9b149d5c
Danil <danil.komyshev777@gmail.com>
2023-07-18 00:49:14 +0300

    [ADD] leader positions to history

--- leader.py
+++ leader.py
@@ -1,5 +1,6 @@
 import asyncio
 import json
+import math
 from datetime import datetime
 
 import requests
@@ -14,6 +15,130 @@ init_data = {}
 host = settings.host
 terminal_path = settings.terminal_path
 exchange_id = settings.exchange_id
+
+
+async def send_history_position(position_ticket, max_balance):
+    url = settings.host + f'position/get/{settings.exchange_id}/{position_ticket}'
+    response = await get(url)
+    url = settings.host + f'option/get/{exchange_id}'
+    options = (await get(url=url))[-1]
+    if response:
+        investment = response[0]['investment_size']
+    else:
+        investment = 0
+    slippage_percent = options['deal_in_plus'] if options['deal_in_plus'] \
+        else options['deal_in_minus']
+    drawdown = (max_balance - (Terminal.get_account_balance() - investment)) / max_balance
+    history_orders = Terminal.get_history_orders_for_ticket(int(position_ticket))
+    deals_time = [deal.time_done for deal in history_orders]
+    date_open = deals_time[0]
+    date_close = deals_time[-1]
+    # datetime.fromtimestamp(deal.time).strftime('%m/%d/%Y %H:%M:%S')
+    deals_price = [deal.price_current for deal in history_orders]
+    price_open = deals_price[0]
+    price_close = deals_price[-1]
+    change_percent = round(math.fabs(price_close - price_open) / price_open, 2)
+
+    volumes = [deal.volume_initial for deal in history_orders]
+    result_volume = 0
+    for vol in volumes[1:]:
+        result_volume = volumes[0] - vol
+    volume_percent = (1 - result_volume / volumes[0]) * 100
+    # print(volumes, volumes[1:])
+
+    position = history_orders[0]
+    if not position:
+        return
+    volume = history_orders[-1].volume_initial
+
+    rates = Terminal.copy_rates_range(position.symbol, date_open - 1, date_close)
+    if len(rates):
+        price_max = max([_[2] for _ in rates])
+        price_min = min([_[3] for _ in rates])
+    else:
+        price_min = price_max = 0
+    history_deals = Terminal.get_history_deals_for_ticket(int(position_ticket))
+    if history_deals:
+        fee = history_deals[-1].fee
+        swap = history_deals[-1].swap
+    else:
+        fee = swap = 0
+    costs = fee + swap
+    profits = [deal.profit for deal in history_deals]
+    gross_pl = sum(profits)
+    net_pl = gross_pl + costs
+    balance = investment + net_pl
+
+    current_positions = Terminal.get_positions()
+    current_profit = 0
+    for pos in current_positions:
+        current_profit += pos.profit
+    float_pl = Terminal.get_account_balance() - current_profit
+    equity = Terminal.get_account_balance() + current_profit
+    size = volume * Terminal.get_contract_size(position.symbol) * price_open
+    bal_net = balance - net_pl
+    lever = size / bal_net if bal_net else 0
+    balance_percent = lever * 100
+    if position.type == 0:  # BUY
+        minimum = (price_min / price_open) * lever * 100 * -1
+        maximum = (price_max / price_open) * lever * 100 * -1
+    else:  # SELL
+        minimum = (price_max / price_open) * lever * 100 * -1
+        maximum = (price_min / price_open) * lever * 100 * -1
+
+    data = {
+        'ticket': position.ticket,  # Ticket
+        'exchange': 'MT5',  # Exchange
+        'user_id': '',  #
+        'api_key': init_data['login'],  # API Key
+        'secret_key': init_data['password'],  # Secret Key
+        'account': str(exchange_id),  # Account
+        'strategy': '',  #
+        'investment': 0,  #
+        'multiplicator': options['multiplier_value'],  # Multiplicator
+        'stop_out': options['stop_value'],  # Stop out
+        'symbol': position.symbol,  # Symbol
+        'type': '',  #
+        'position': '',  #
+        'side': 'buy' if position.type == 0 else 'sell' if position.type == 1 else 'not buy or sell',  # Side
+        'currency': '',  #
+        'slippage_percent': slippage_percent,  # Slippage %
+        'slippage_time': options['waiting_time'],  # Slippage time
+        'size': size,  # Size
+        'lots': volume,  # Lots
+        'lever': lever,  # Lever
+        'balance_percent': balance_percent,  # Balance %
+        'volume_percent': volume_percent,  # Volume %
+        'open_time': date_open,  # Open Time
+        'open_price': price_open,  # Open Price
+        'stop_loss': response[0]['sl'] if response else 0,  # history_orders[-1].sl,  # Stop loss
+        'take_profit': response[0]['tp'] if response else 0,  # history_orders[-1].tp,  # Take profit
+        'close_time': date_close,  # Close time
+        'close_price': price_close,  # Close Price
+        'change_percent': change_percent,  # Change_%
+        'gross_p_l': gross_pl,  # Gross P&L
+        'commision': fee,  # Fee
+        'swap': swap,  # Swap
+        'costs': costs,  # Costs
+        'net_p_l': net_pl,  # Net P&L
+        'roi': 0,  #
+        'balance': balance,  # Balance
+        'equity': equity,  # Equity
+        'float_p_l': float_pl,  # Float P&L
+        'duration': 0,  #
+        'drawdown': drawdown,  # Drawdown
+        'minimum': minimum,  # Minimum
+        'maximum': maximum,  # Maximum
+        'risk_reward': 0,  # `
+        'roi_missed': 0,  # `
+        'slip_percent': 0,  # `
+        'slip_time': 0,  # `
+        'magic': str(position.magic),  # Magic
+        'comment': position.comment,  # Comment
+    }
+    print('history_position:\n', data)
+    url = settings.host + 'position-history/post'
+    await post(url, data=json.dumps(data))
 
 
 def get_settings(exchange_idx):
@@ -111,6 +236,7 @@ async def update_leader_info(sleep=settings.sleep_leader_update):
         for position in active_db_positions:
             if int(position['ticket']) not in terminal_tickets:
                 await disable_position(exchange_id, int(position['ticket']))
+                await send_history_position(position['ticket'], leader_balance)
         for position in terminal_positions:
             if position.ticket not in active_db_tickets:
                 await send_position(position)
